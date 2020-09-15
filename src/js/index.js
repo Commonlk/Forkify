9df7fa49b47ee861d725ec1d00c42324daec2e83
Jon Victor <vitinsilva0xd@gmail.com>
2020-09-15 19:11:59 -0300

    Add search functionality and other minor changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
-import $ from "jquery";
 import Search from "./models/Search";
+import Recipe from "./models/Recipe";
+import * as searchView from "./views/SearchView";
+import { elements, renderLoader, clearLoader } from "./views/base";
 
 /*
 Global state of the app
@@ -11,25 +13,66 @@ Global state of the app
 
 const state = {};
 
+// SEARCH CONTROLLER
+
 const controlSearch = async () => {
   // 1- Get query from view
-  const query = "pizza"; // TODO
+  const query = searchView.getInput(); // TODO
 
   if (query) {
     // 2- New search object and add to state
     state.search = new Search(query);
 
     // 3- Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
 
     // 4- Search for recipes
     await state.search.getResults();
 
     // 5- Render results on the UI
-    console.log(state.search.result);
+    clearLoader();
+    searchView.renderResults(state.search.result);
   }
 };
 
-$(".search").on("submit", (e) => {
+elements.searchForm.on("submit", (e) => {
   e.preventDefault();
   controlSearch();
 });
+
+elements.searchResPages.on("click", (e) => {
+  const btn = e.target.closest(".btn-inline");
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+  }
+});
+
+// RECIPE CONTROLLER
+
+const controlRecipe = async () => {
+  // Get ID from url
+  const id = window.location.hash.replace("#", "");
+  console.log(id);
+  if (id) {
+    // Prepare UI for changes
+
+    // Create new recipe object
+    state.recipe = new Recipe(id);
+
+    // Get recipe data
+    await state.recipe.getRecipe();
+
+    // Calculate servings and time
+    state.recipe.calcTime();
+    state.recipe.calcServings();
+
+    // Render recipe
+    console.log(state.recipe);
+  }
+};
+
+window.addEventListener("hashchange", controlRecipe);
