5bc99634838d9b38da6283181c34b249e9b05044
Jon Victor <vitinsilva0xd@gmail.com>
2020-09-15 12:00:39 -0300

    Add basic funtionality to search

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,35 @@
-// Global app controller
-import num from "./test";
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! ${x}`);
+import $ from "jquery";
+import Search from "./models/Search";
+
+/*
+Global state of the app
+* - Search object
+* - Current recipe object
+* - Shopping list object
+* - Liked recipes
+*/
+
+const state = {};
+
+const controlSearch = async () => {
+  // 1- Get query from view
+  const query = "pizza"; // TODO
+
+  if (query) {
+    // 2- New search object and add to state
+    state.search = new Search(query);
+
+    // 3- Prepare UI for results
+
+    // 4- Search for recipes
+    await state.search.getResults();
+
+    // 5- Render results on the UI
+    console.log(state.search.result);
+  }
+};
+
+$(".search").on("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
