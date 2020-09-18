390d9fc6efed5743146e53ea376ebc4a03ab1caf
Victor Azevedo <vitinsilva0xd@gmail.com>
2020-09-18 20:24:01 -0300

    Fix deleteItem function

--- src/js/models/List.js
+++ src/js/models/List.js
@@ -20,7 +20,7 @@ export default class List {
     const index = this.items.findIndex((el) => el.id === id);
     // [2,4,8] splice(1,2) -> return [4,8], original array is [2,8]
     // [2,4,8] slice(1,2) -> return 4, original array is [2,4,8]
-    this.item.splice(index, 1);
+    this.items.splice(index, 1);
   }
 
   updateCount(id, newCount) {
