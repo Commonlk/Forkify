b95458298b35cbf8416e53cff602c669a9a1d975
Victor Azevedo <vitinsilva0xd@gmail.com>
2020-09-21 16:35:40 -0300

    Stop implementing likes feature

--- src/js/index.js
+++ src/js/index.js
@@ -136,9 +136,7 @@ elements.shopping.on("click", (e) => {
     state.list.updateCount(id, val);
   }
 });
-// TESTING
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
+
 // LIKE CONTROLLER
 const controlLike = () => {
   if (!state.likes) {
@@ -171,6 +169,22 @@ const controlLike = () => {
   }
   likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
+
+// Restore liked recipes on page load
+window.addEventListener("load", () => {
+  state.likes = new Likes();
+
+  // Restore likes
+  state.likes.readStorage();
+
+  // Toggle like menu button
+  likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+  // Render existing likes
+  state.likes.likes.forEach((like) => {
+    likesView.renderLike(like);
+  });
+});
 
 // Handling recipe button clicks
 elements.recipe.on("click", (e) => {
