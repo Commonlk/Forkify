87eb452600586608ab09c59d328f28ca11be625a
Victor Azevedo <vitinsilva0xd@gmail.com>
2020-09-21 13:11:28 -0300

    Fix possible casing bug

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import Search from "./models/Search";
 import Recipe from "./models/Recipe";
 import List from "./models/List";
 import Likes from "./models/Likes";
-import * as searchView from "./views/SearchView";
+import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
 import * as listView from "./views/listView";
 import * as likesView from "./views/likesView";
